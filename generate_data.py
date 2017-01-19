5d649e6d483a29209290d3979a1a0c3be5d61132
Moacir P. de Sá Pereira <moacir@gmail.com>
2017-01-19 06:19:28 -0500

    add link id.

--- generate_data.py
+++ generate_data.py
@@ -14,40 +14,40 @@ books = [{} for _ in range(0, total)]
 for i, member in enumerate(members):
     member['name'] = fake.name()
     member['books'] = rentals[i]
-    member['id'] = "member_" + str(i + 1)
+    member["id"] = "member_" + str(i + 1)
 for i, book in enumerate(books):
     book['title'] = " ".join(fake.words(np.random.randint(1,6))).title()
-    book['id'] = "book_" + str(i + 1)
+    book["id"] = "book_" + str(i + 1)
 events = []
 for member in members:
     for _ in range(0, member['books']):
-        events.append([member['id'], np.random.choice(books, 1, p=book_ps)[0]['id']])
+        events.append([member["id"], np.random.choice(books, 1, p=book_ps)[0]["id"]])
 book_links = []
 for book in books:
-    members_who_borrowed = set([event[0] for event in events if event[1] == book['id']])
+    members_who_borrowed = set([event[0] for event in events if event[1] == book["id"]])
     books_in_common = set([event[1] for event in events if event[0] in members_who_borrowed])
     for other_book in books_in_common:
-        if(book['id'] != other_book):
-            this_link = next((link for link in book_links if (link["source"] == book['id'] and link["target"] == other_book)), None)
+        if(book["id"] != other_book):
+            this_link = next((link for link in book_links if (link["source"] == book["id"] and link["target"] == other_book)), None)
             if this_link == None:
-                inverse_link = next((link for link in book_links if (link["target"] == book['id'] and link["source"] == other_book)), None)
+                inverse_link = next((link for link in book_links if (link["target"] == book["id"] and link["source"] == other_book)), None)
                 if inverse_link == None:
                     members_who_borrowed_other_book = set([event[0] for event in events if event[1] == other_book])
                     common_members = [member for member in members_who_borrowed if member in members_who_borrowed_other_book]
-                    book_links.append({"source": book['id'], "target": other_book, "members": common_members, "value": len(common_members)})
+                    book_links.append({"id": book["id"] + "_" + other_book, "source": book["id"], "target": other_book, "members": common_members, "value": len(common_members)})
 member_links = []
 for member in members:
-    borrowed_books = set([event[1] for event in events if event[0] == member['id']])
+    borrowed_books = set([event[1] for event in events if event[0] == member["id"]])
     members_in_common = set([event[0] for event in events if event[1] in borrowed_books])
     for other_member in members_in_common:
-        if(member['id'] != other_member):
-            this_link = next((link for link in member_links if (link["source"] == member['id'] and link["target"] == other_member)), None)
+        if(member["id"] != other_member):
+            this_link = next((link for link in member_links if (link["source"] == member["id"] and link["target"] == other_member)), None)
             if this_link == None:
-                inverse_link = next((link for link in member_links if (link["target"] == member['id'] and link["source"] == other_member)), None)
+                inverse_link = next((link for link in member_links if (link["target"] == member["id"] and link["source"] == other_member)), None)
                 if inverse_link == None:
                     books_borrowed_other_member = set([event[1] for event in events if event[0] == other_member])
                     common_books = [book for book in borrowed_books if book in books_borrowed_other_member]
-                    member_links.append({"source": member['id'], "target": other_member, "value": len(common_books), "books": common_books})
+                    member_links.append({"id": member["id"] + "_" + other_member, "source": member["id"], "target": other_member, "value": len(common_books), "books": common_books})
 with open("books.json", "w") as outfile:
     json.dump(books, outfile)
 with open("book_links.json", "w") as outfile:
